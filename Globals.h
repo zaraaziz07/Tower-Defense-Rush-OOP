31b71f837160a04b8a35ae0b38a0e0596dd6d3b5
Zara Aziz <zaraaziz2007@gmail.com>
2026-05-12 21:08:51 +0500

    Update Globals.h

--- Globals.h
+++ Globals.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -11,6 +12,14 @@ int grid[rows][cols] = { 0 };
 const int TileSize = 32;
 const int MaxEnemiesPerWave = 15;
 const int totalWaves = 5;
+int coins = 500;
+int lives =5;
+string selectedTower = "";
+bool placingTower = false;
+int selectedCost = 0;
+class Tower;
+Tower* placedTowers[100];
+int towerCount = 0;
 class Enemy;
 
 class WayPoint {
@@ -20,7 +29,6 @@ public:
 };
 
 
-
 class Wave {
 public:
     Enemy* enemies[MaxEnemiesPerWave];
@@ -28,8 +36,8 @@ public:
     float spawnInterval;   //Seconds between each spawn
     int currentSpawn;      //Which enemy to spawn next
     float spawnTimer;      //Time since last spawn
-    Wave() : enemyCount(0), spawnInterval(2.0f),
-    currentSpawn(0), spawnTimer(0)
+    Wave() : enemyCount(0), spawnInterval(3.0f),
+        currentSpawn(0), spawnTimer(0)
     {
         for (int i = 0; i < MaxEnemiesPerWave; i++)
             enemies[i] = nullptr;
@@ -44,6 +52,7 @@ public:
     }
 };
 
-extern Wave waves[totalWaves];
+
+
 
 
